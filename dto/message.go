a070484e88f0764dfcca3dbdddbd8cf11f1e1d6f
Alex <akh.alex.khan@gmail.com>
2020-01-06 12:34:23 +0200

    Add protocol versioning to message dto

--- dto/message.go
+++ dto/message.go
@@ -12,40 +12,11 @@ type Content struct {
 // Message - это данные от устройства прошедшие валидацию и разделенные на содержимое и команду
 type Message struct {
 	SessionID uint32
+	Proto     uint16
+	Jmp       uint16
 	Command   uint16
 	Content   []Content
 }
-
-// // FormMessageAnswer - Формирует сообщение из входных аргументов
-// func FormMessageAnswer(command uint16, content ...string) Message {
-// 	var res []Content
-// 	for _, str := range content {
-// 		t := Content{Data: []byte(str)}
-// 		res = append(res, t)
-// 	}
-// 	return Message{
-// 		Command: command,
-// 		Content: res,
-// 	}
-// }
-
-// // Copy - копирует одно сообщение в другое
-// func Copy(dst, src *Message) error {
-// 	if dst == nil || src == nil {
-// 		return fmt.Errorf("Message is nil")
-// 	}
-// 	dst.Command = src.Command
-// 	dst.SessionID = src.SessionID
-// 	copy(dst.Content, src.Content)
-// 	return nil
-// }
-
-// // Clear - очищает сообщение
-// func Clear(m *Message) {
-// 	m.Command = 0
-// 	m.SessionID = 0
-// 	m.Content = m.Content[:0]
-// }
 
 // ClientDescriptor - base entity for client to client messanger
 type ClientDescriptor struct {
--- parser/c2cParser.go
+++ parser/c2cParser.go
@@ -36,18 +36,21 @@ var delim = []byte(";")
 var minHeaderLen = len(beginHeader) + headerParamSize*len(delim) + len(endHeader)
 
 type header struct {
-	headerSize  int
-	from        string
-	to          string
-	mType       uint64
-	jumpCnt     uint64
+	protocolVer uint64 // Версия протокола
+	mType       uint64 // Тип сообщения (смотри клиента)
+	jumpCnt     uint64 // счетчик прыжков
+
+	headerSize  int // Размер заголовка
 	contentSize int // Размер данных
+
+	from string
+	to   string
 }
 
 // C2cParser - Парсер разбирает сообщения по протоколу
 // 1 - клиент-клиент
 type C2cParser struct {
-	protocolVer uint64
+	head header
 }
 
 //FormMessage - from - Content[0], to - Content[1], data - Content[2]
@@ -57,7 +60,7 @@ func (c2c *C2cParser) FormMessage(msg dto.Message) ([]byte, error) {
 	}
 	res := make([]byte, 0, 128+len(msg.Content[0].Data))
 	res = append(res, []byte(beginHeader)...)
-	res = append(res, []byte(strconv.FormatUint(c2c.protocolVer, 16))...)
+	res = append(res, []byte(strconv.FormatUint(c2c.head.protocolVer, 16))...)
 	res = append(res, ';')
 	res = append(res, msg.Content[0].Data...)
 	res = append(res, ';')
@@ -73,92 +76,89 @@ func (c2c *C2cParser) FormMessage(msg dto.Message) ([]byte, error) {
 	return res, nil
 }
 
-// return size of header and error if not find header or parsing error
-func (c2c *C2cParser) parseHeader(data []byte) (int, header, error) {
-	var resHeader header
+// return position for start header or/and error if not find header or parsing error
+func (c2c *C2cParser) parseHeader(data []byte) (int, error) {
 	if data == nil || len(data) < minHeaderLen {
-		return 0, resHeader, fmt.Errorf("Input is empty, nothing to be parsed")
+		return 0, fmt.Errorf("Input is empty, nothing to be parsed")
 	}
 	index := bytes.IndexByte(data, '$')
 	if index < 0 {
-		return 0, resHeader, fmt.Errorf("Undefined start symb of package")
+		return 0, fmt.Errorf("Undefined start symb of package")
 	}
 	if !bytes.EqualFold(data[index:index+2], beginHeader) {
-		return index, resHeader, fmt.Errorf("Package must be started from %s", beginHeader)
+		return index, fmt.Errorf("Package must be started from %s", beginHeader)
 	}
-	resHeader.headerSize = bytes.Index(data, []byte(endHeader)) // Поиск конца заголовка
-	if resHeader.headerSize < index || resHeader.headerSize >= len(data) {
-		return index, resHeader, fmt.Errorf("Undefined end header %s", endHeader)
+	c2c.head.headerSize = bytes.Index(data, []byte(endHeader)) // Поиск конца заголовка
+	if c2c.head.headerSize < index || c2c.head.headerSize >= len(data) {
+		return index, fmt.Errorf("Undefined end header %s", endHeader)
 	}
-	parsed := bytes.Split(data[index+2:resHeader.headerSize], delim)
+	parsed := bytes.Split(data[index+2:c2c.head.headerSize], delim)
 	if len(parsed) < headerParamSize {
-		return index, resHeader, fmt.Errorf("Incorrect header")
+		return index, fmt.Errorf("Incorrect header")
 	}
 	var err error
-	if c2c.protocolVer, err = strconv.ParseUint(string(parsed[0]), 16, 64); err != nil { //Версия протокола
+	if c2c.head.protocolVer, err = strconv.ParseUint(string(parsed[0]), 16, 64); err != nil { //Версия протокола
 		log.Tracef("Can not parse number in %s error %s", parsed[0], err.Error())
-		return index, resHeader, fmt.Errorf("Icorrect protocol version, it must be a number")
+		return index, fmt.Errorf("Icorrect protocol version, it must be a number")
 	}
-	switch c2c.protocolVer {
+	switch c2c.head.protocolVer {
 	case 0: // Для сервер-сервер соединения
 		fallthrough
 	case 1: // Для клиент-сервер соединения
-		resHeader.from = string(parsed[1])                                                   // от кого
-		resHeader.to = string(parsed[2])                                                     //кому
-		if resHeader.mType, err = strconv.ParseUint(string(parsed[3]), 16, 64); err != nil { //тип сообщения (команда)
-			return index, resHeader, fmt.Errorf("Icorrect message type, it must be a number")
+		c2c.head.from = string(parsed[1])                                                   // от кого
+		c2c.head.to = string(parsed[2])                                                     //кому
+		if c2c.head.mType, err = strconv.ParseUint(string(parsed[3]), 16, 64); err != nil { //тип сообщения (команда)
+			return index, fmt.Errorf("Icorrect message type, it must be a number")
 		}
-		if resHeader.jumpCnt, err = strconv.ParseUint(string(parsed[4]), 16, 64); err != nil {
-			return index, resHeader, fmt.Errorf("Incorrect message jump type")
+		if c2c.head.jumpCnt, err = strconv.ParseUint(string(parsed[4]), 16, 64); err != nil {
+			return index, fmt.Errorf("Incorrect message jump type")
 		}
-		if resHeader.jumpCnt == 0 {
-			return index, resHeader, fmt.Errorf("Jump count is zero")
+		if c2c.head.jumpCnt == 0 {
+			return index, fmt.Errorf("Jump count is zero")
 		}
 		var s uint64
 		if s, err = strconv.ParseUint(string(parsed[5]), 16, 64); err != nil { //размер сообщения
-			return index, resHeader, fmt.Errorf("Icorrect message size, it must be a number")
+			return index, fmt.Errorf("Icorrect message size, it must be a number")
 		}
 		if s > maxPackageSzie {
 			err := fmt.Errorf("Income package is too big. Overflow internal buffer")
 			log.Error(err.Error())
-			return index, resHeader, err
+			return index, err
 		}
-		resHeader.contentSize = int(s)
-		resHeader.headerSize += len(endHeader) // Add endHeader
-		return index, resHeader, nil
+		c2c.head.contentSize = int(s)
+		c2c.head.headerSize += len(endHeader) // Add endHeader
+		return index, nil
 		// TODO implement another version of protocol
 	default:
-		return index, resHeader, fmt.Errorf("Error usuported porotocol")
+		return index, fmt.Errorf("Error usuported porotocol")
 	}
 }
 
 //ParseMessage - from - Content[0], to - Content[1], data - Content[2]
 func (c2c *C2cParser) ParseMessage(data []byte) (dto.Message, error) {
 	var err error
-	var head header
 	var i int
-	if i, head, err = c2c.parseHeader(data); err != nil {
+	if i, err = c2c.parseHeader(data); err != nil {
 		log.Trace(err.Error())
 		return dto.Message{}, err
 	}
-	log.Tracef("Message from %s to %s type %d jump %d and content size %d", head.from, head.to, head.mType, head.jumpCnt, head.contentSize)
-	head.jumpCnt--
+	log.Tracef("Message from %s to %s type %d jump %d and content size %d", c2c.head.from, c2c.head.to, c2c.head.mType, c2c.head.jumpCnt, c2c.head.contentSize)
+	c2c.head.jumpCnt--
 	jmp := make([]byte, 8)
-	binary.LittleEndian.PutUint64(jmp, head.jumpCnt)
+	binary.LittleEndian.PutUint64(jmp, c2c.head.jumpCnt)
 	return dto.Message{
-		Command: uint16(head.mType),
+		Command: uint16(c2c.head.mType),
+		Proto:   uint16(c2c.head.protocolVer),
+		Jmp:     uint16(c2c.head.jumpCnt),
 		Content: []dto.Content{
-			dto.Content{
-				Data: []byte(head.from),
+			dto.Content{ // from address
+				Data: []byte(c2c.head.from),
 			},
-			dto.Content{
-				Data: []byte(head.to),
+			dto.Content{ // to address
+				Data: []byte(c2c.head.to),
 			},
-			dto.Content{
-				Data: data[i+head.headerSize : i+head.headerSize+head.contentSize],
-			},
-			dto.Content{
-				Data: jmp,
+			dto.Content{ // All data
+				Data: data[i+c2c.head.headerSize : i+c2c.head.headerSize+c2c.head.contentSize],
 			},
 		},
 	}, nil
@@ -168,13 +168,16 @@ func (c2c *C2cParser) ParseMessage(data []byte) (dto.Message, error) {
 // TODO каждый раз парсить заголовок не эффективно надо будет переписать
 func (c2c *C2cParser) IsFullReceiveMsg(data []byte) (bool, error) {
 	var err error
-	var head header
 	var i int
-	if i, head, err = c2c.parseHeader(data); err != nil {
+	//TODO check it later
+	if (c2c.head.headerSize+c2c.head.contentSize > 0) && (len(data) > c2c.head.headerSize+c2c.head.contentSize) {
+		return true, nil
+	}
+	if i, err = c2c.parseHeader(data); err != nil {
 		log.Trace(err.Error())
 		return false, err
 	}
-	if len(data) >= i+head.contentSize+head.headerSize {
+	if len(data) >= i+c2c.head.contentSize+c2c.head.headerSize {
 		return true, nil
 	}
 	return false, nil
