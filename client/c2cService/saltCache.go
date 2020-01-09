963fd64ef025a05ed1400e05bfba82c931b48188
Alex <akh.alex.khan@gmail.com>
2020-01-10 00:26:24 +0200

    delete mutex in salt cache

--- client/c2cService/saltCache.go
+++ client/c2cService/saltCache.go
@@ -1,9 +1,11 @@
 package c2cService
 
-import "sync"
+import (
+	"strconv"
+)
 
 var saltCache map[string]int // Кеш случайных солей и кол-во раз котрое они встречались там
-var saltMtx sync.RWMutex
+// var saltMtx sync.RWMutex
 
 func init() {
 	saltCache = make(map[string]int)
@@ -12,33 +14,27 @@ func init() {
 // CheckSaltByUserName Функция возвращает сколько раз использовалась одна и та же соль
 // при авторизации конкретного пользователя
 func CheckSaltByUserName(name, salt string) int {
-	saltMtx.RLock()
-	cnt, ok := saltCache[salt]
-	saltMtx.RUnlock()
+	s := name + salt
+	cnt, ok := saltCache[s]
 	if !ok {
 		cnt = 1
 	} else {
 		cnt++
 	}
-	saltMtx.Lock()
-	saltCache[salt] = cnt
-	saltMtx.Unlock()
+	saltCache[s] = cnt
 	return cnt
 }
 
 // CheckSaltByID Функция возвращает сколько раз использовалась одна и та же соль
 // при авторизации конкретного пользователя
 func CheckSaltByID(ID uint64, salt string) int {
-	saltMtx.RLock()
-	cnt, ok := saltCache[salt]
-	saltMtx.RUnlock()
+	s := strconv.FormatUint(ID, 16) + salt
+	cnt, ok := saltCache[s]
 	if !ok {
 		cnt = 1
 	} else {
 		cnt++
 	}
-	saltMtx.Lock()
-	saltCache[salt] = cnt
-	saltMtx.Unlock()
+	saltCache[s] = cnt
 	return cnt
 }
