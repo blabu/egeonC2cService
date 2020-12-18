20a4d2ea5abc1dc7892cd5f16e6a632546e2fe76
Alex Khanin <akh.alex.khan@gmail.com>
2020-12-18 17:19:11 +0200

    Move library into separete repository

--- dto/readHandler.go
+++
@@ -1,7 +0,0 @@
-package dto
-
-// ClientReadHandler - стандартный обработчик для чтения пакета между узлами внутри системы
-type ClientReadHandler func(msg Message, err error) error
-
-//ServerReadHandler - обработчик чтания данных с системы для отправки в интернет
-type ServerReadHandler func([]byte, error) error
